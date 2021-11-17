528fc131e0e9f7c7fefc8f2099a6efb8d59dab22
薛斌 <928691436@qq.com>
2021-11-17 21:27:25 +0800

    deploy

---
+++ precache-manifest.5ffdb2c75efc6bb8829370382e8ff627.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a0f31b33d5a1f5722c",
+    "url": "/accounting-web/css/app.48cbd4ca.css"
+  },
+  {
+    "revision": "f66dc3d797c1628d6ef9",
+    "url": "/accounting-web/css/chunk-vendors.0088110d.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/accounting-web/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/accounting-web/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "e8f76f88a27f5200453f5e7d05ad725a",
+    "url": "/accounting-web/index.html"
+  },
+  {
+    "revision": "80a0f31b33d5a1f5722c",
+    "url": "/accounting-web/js/app.c7cbd0ee.js"
+  },
+  {
+    "revision": "f66dc3d797c1628d6ef9",
+    "url": "/accounting-web/js/chunk-vendors.dc6b9649.js"
+  },
+  {
+    "revision": "e09939e8bdb3bc07e427e0d8dbf41c91",
+    "url": "/accounting-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-web/robots.txt"
+  }
+]);
